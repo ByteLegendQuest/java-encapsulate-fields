29b408b6bd7fec595ece81aae9d85640b5408733
saveunhappy <925147954@qq.com>
2022-07-05 15:41:16 +0000

    Update at 2022-07-05T15:41:16.948980Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -5,4 +5,36 @@ public class Cat {
     public int age;
     public String color;
     public boolean cute;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isCute() {
+        return cute;
+    }
+
+    public void setCute(boolean cute) {
+        this.cute = cute;
+    }
 }
