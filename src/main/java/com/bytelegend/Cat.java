737c41229c412248d48131efb3130cc7979b5854
Anlyui <1391469813@qq.com>
2022-08-19 03:05:38 +0000

    Update at 2022-08-19T03:05:38.380525Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -1,8 +1,36 @@
 package com.bytelegend;
 
 public class Cat {
-    public String name;
-    public int age;
-    public String color;
-    public boolean cute;
+    private String name;
+    private int age;
+    private String color;
+    private boolean cute;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isCute() {
+        return cute;
+    }
+    public void setCute(boolean cute) {
+        this.cute = cute;
+    }
 }
