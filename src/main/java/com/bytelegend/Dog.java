ff43c120ad6ae3602567f3a7ee4ae2dd1bf92e35
pamaomaoa <544060246@qq.com>
2022-11-20 15:33:00 +0000

    Update at 2022-11-20T15:33:00.271685Z

--- src/main/java/com/bytelegend/Dog.java
+++ src/main/java/com/bytelegend/Dog.java
@@ -1,12 +1,76 @@
 package com.bytelegend;
 
 public class Dog {
-    public String name;
-    public String gender;
-    public int age;
-    public String color;
-    public String breed;
-    public int size;
-    public int weight;
-    public boolean cute;
+    private String name;
+    private String gender;
+    private int age;
+    private String color;
+    private String breed;
+    private int size;
+    private int weight;
+    private boolean cute;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public boolean isCute() {
+        return cute;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setCute(boolean cute) {
+        this.cute = cute;
+    }
 }
