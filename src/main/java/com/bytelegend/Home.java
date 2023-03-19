c02cbbe0be6c2c05a800cb39ae8165482d6a8e00
guakun01 <guakun01@gmail.com>
2023-03-19 04:43:04 +0000

    Update at 2023-03-19T04:43:04.498441Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,6 +1,21 @@
 package com.bytelegend;
 
 public class Home {
-    public Cat cat;
-    public Dog dog;
+    private Cat cat;
+    private Dog dog;
+    public Cat getCat() {
+        return cat;
+    }
+
+    public void setCat(Cat cat) {
+        this.cat = cat;
+    }
+
+    public Dog getDog() {
+        return dog;
+    }
+
+    public void setDog(Dog dog) {
+        this.dog = dog;
+    }
 }
